c4614ff62853dfed8c03778a10147d6e0b138cf6
lvb2104 <levanbao2105@gmail.com>
2024-11-20 00:04:42 +0700

    Set up guards for api-key

---
+++ src/common/guards/api-key/api-key.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<Request>();
+
+    const authHeader = request.header('Authorization');
+    return authHeader === (new ConfigService()).getOrThrow('API_KEY');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -16,7 +17,8 @@ async function bootstrap() {
             },
         }),
     );
-    app.useGlobalFilters(new HttpExceptionFilter());
+    // app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalGuards(new ApiKeyGuard)
     const configService = app.get(ConfigService);
     const PORT = configService.get('PORT');
     await app.listen(PORT);
